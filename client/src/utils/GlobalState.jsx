68a105c305293a6cf685779b52cf800c1b27c913
Nathan Geronimo <nathangero@yahoo.com>
2023-12-07 16:54:11 -0800

    refactor GlobalState to use redux store

--- client/src/utils/GlobalState.jsx
+++ client/src/utils/GlobalState.jsx
@@ -1,23 +1,7 @@
-import { createContext, useContext, useReducer } from "react";
-import { reducer } from './reducers'
+import { Provider } from 'react-redux' ;
+import store from './store'
 
-const StoreContext = createContext();
-const { Provider } = StoreContext;
 
-const StoreProvider = ({ value = [], ...props }) => {
-  const [state, dispatch] = useReducer(reducer, {
-    products: [],
-    cart: [],
-    cartOpen: false,
-    categories: [],
-    currentCategory: '',
-  });
-
-  return <Provider value={[state, dispatch]} {...props} />;
-};
-
-const useStoreContext = () => {
-  return useContext(StoreContext);
-};
-
-export { StoreProvider, useStoreContext };
+export default function StoreProvider(props) {
+  return <Provider store={store} {...props} />;
+}
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -10,8 +10,16 @@ import {
   TOGGLE_CART,
 } from './actions';
 
+const initialState = {
+  products: [],
+  cart: [],
+  cartOpen: false,
+  categories: [],
+  currentCategory: '',
+}
+
 // TODO: To get a better understand of how a reducer works - add comments to the various actions in the reducer
-export const reducer = (state, action) => {
+export const reducer = (state = initialState, action) => {
   switch (action.type) {
     // TODO: Add a comment describing the functionality of the UPDATE_PRODUCTS case
     // Your comment here
